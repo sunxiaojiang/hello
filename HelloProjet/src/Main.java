f568ec2c4ba378c885d4cd17316200f14635f03c
sunxiaojiang <sunxiao_jiang@163.com>
2019-08-07 09:19:02 +0800

    笔记本提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("HelloWorld!4");
         System.out.println("HelloWorld!4");
         System.out.println("HelloWorld!6");
+        System.out.println("HelloWorld!6");
 
 
     }
